5d4b42122cc457e649096ff190f80a3dd882af15
leonidk <e_leokim@yahoo.com>
2021-09-17 10:08:00 +0300

    Run fmt

--- nessie.config.ts
+++ nessie.config.ts
@@ -1,19 +1,19 @@
 import {
-  ClientPostgreSQL,
-  NessieConfig,
-} from "https://deno.land/x/nessie@2.0.1/mod.ts";
+	ClientPostgreSQL,
+	NessieConfig,
+} from 'https://deno.land/x/nessie@2.0.1/mod.ts';
 
 export const client = new ClientPostgreSQL({
-  database: Deno.env.get("DB_NAME"),
-  hostname: Deno.env.get("DB_HOST"),
-  port: Number.parseInt(Deno.env.get("DB_PORT") ?? "5432"),
-  user: Deno.env.get("DB_USER"),
-  password: Deno.env.get("DB_PWD"),
+	database: Deno.env.get('DB_NAME'),
+	hostname: Deno.env.get('DB_HOST'),
+	port: Number.parseInt(Deno.env.get('DB_PORT') ?? '5432'),
+	user: Deno.env.get('DB_USER'),
+	password: Deno.env.get('DB_PWD'),
 });
 
 const config: NessieConfig = {
-  client: client,
-  debug: false,
+	client: client,
+	debug: false,
 };
 
 export default config;
