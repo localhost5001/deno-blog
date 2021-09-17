ed92323e1da8ab6e067ef18c549401cc9058aa53
leonidk <e_leokim@yahoo.com>
2021-09-17 15:11:31 +0300

    Run fmt on dbClient

--- src/dbClient.ts
+++ src/dbClient.ts
@@ -1,9 +1,9 @@
 import { Pool } from 'postgres';
 
-const POOL_SIZE = Number.parseInt(Deno.env.get('DB_POOL_SIZE') ?? '10')
+const POOL_SIZE = Number.parseInt(Deno.env.get('DB_POOL_SIZE') ?? '10');
 
 export const dbConnPool = new Pool({
-  database: Deno.env.get('DB_NAME'),
+	database: Deno.env.get('DB_NAME'),
 	hostname: Deno.env.get('DB_HOST'),
 	port: Number.parseInt(Deno.env.get('DB_PORT') ?? '5432'),
 	user: Deno.env.get('DB_USER'),
